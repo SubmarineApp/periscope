0cd9a99d038b8040ee4eed7781f40a064920a82c
octakaidecagon <octakaidecagon@gmail.com>
2021-02-27 17:57:57 -0600

    "test3"

--- lib/CalendarPage.dart
+++ lib/CalendarPage.dart
@@ -12,16 +12,32 @@ class _CalendarPageState extends State<CalendarPage> {
   //   return Scaffold(
   //     body: SfCalendar(
   //       view: CalendarView.month,
-  //       dataSource: MeetingDataSource(_getDataSource()),
+  //       dataSource: EventDataSource(_getDataSource()),
   //       monthViewSettings: MonthViewSettings(
   //           appointmentDisplayMode: MonthAppointmentDisplayMode.appointment),
   //     ),
   //   );
   // }
+
   Widget build(BuildContext context) {
     return Scaffold(
         body: SfCalendar(
       view: CalendarView.month,
     ));
   }
+
+//   List<Event> _getDataSource() {
+//     events = <Event>[];
+//     final DateTime today = DateTime.now();
+//     final DateTime StartTime =
+//         DateTime(today.year, today.month, today.day, 9, 0, 0);
+//     final DateTime endTime = StartTime.add(const Duration(hours: 2));
+//     events.add(
+//         Event('Event', startTime, endTime, const Color(0xFF0F8644), false));
+//     return events;
+//   }
+// }
+
+// class EventDataSource {
+// }
 }
