389040d41efb33c2a05b70c0fae0c6b6127840d3
Michael Nesbitt <nesbitt@treeman.nesbitt.rocks>
2021-03-11 11:17:56 -0600

    Changed grid layout to allow larger tiles

--- lib/OverviewPage.dart
+++ lib/OverviewPage.dart
@@ -3,9 +3,9 @@ import 'dart:collection';
 import 'package:backend_api/api.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:jiffy/jiffy.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class OverviewPage extends StatefulWidget {
   final DefaultApi client;
@@ -30,7 +30,7 @@ class _OverviewPageState extends State<OverviewPage> {
   List<MonthlySpendingAccumulator> _monthlySpending = [];
   Map<Category, List<MonthlySpendingAccumulator>> _categoryMonthlySpending =
       new HashMap<Category, List<MonthlySpendingAccumulator>>();
-  List<charts.Color> _colors = [
+  final List<charts.Color> _colors = [
     charts.Color(a: 0xFF, b: 0xA6, g: 0x5D, r: 0x90),
     charts.Color(a: 0xFF, b: 0x7A, g: 0x5D, r: 0xA6),
     charts.Color(a: 0xFF, b: 0x5D, g: 0x9E, r: 0xA6),
@@ -38,6 +38,7 @@ class _OverviewPageState extends State<OverviewPage> {
     charts.Color(a: 0xFF, b: 0xA6, g: 0x8A, r: 0x5D),
     charts.Color(a: 0xFF, b: 0xA6, g: 0x5D, r: 0x90),
   ];
+  double _totalAdjustedMonthlySpending = 0;
 
   _OverviewPageState({this.client, this.subscriptions, this.categories}) {
     _init();
@@ -131,6 +132,9 @@ class _OverviewPageState extends State<OverviewPage> {
       });
       return MapEntry(category, monthlySpendingForCategory);
     });
+    _categorySpendingThisMonth.forEach((value) {
+      _totalAdjustedMonthlySpending += value.amount;
+    });
     setState(() {});
   }
 
@@ -149,8 +153,14 @@ class _OverviewPageState extends State<OverviewPage> {
     final simpleCurrencyFormatter =
         new charts.BasicNumericTickFormatterSpec.fromNumberFormat(
             new NumberFormat.compactSimpleCurrency());
-    return GridView.extent(
-      maxCrossAxisExtent: 500,
+    return StaggeredGridView.count(
+      crossAxisCount: 4,
+      staggeredTiles: [
+        const StaggeredTile.count(1, 1),
+        const StaggeredTile.count(1, 1),
+        const StaggeredTile.count(1, 1),
+        const StaggeredTile.count(2, 1),
+      ],
       children: [
         Container(
           padding: EdgeInsets.all(16),
@@ -182,30 +192,27 @@ class _OverviewPageState extends State<OverviewPage> {
             direction: Axis.vertical,
             children: [
               Text(
-                "Adjusted Monthly Spending by Category",
+                "Adjusted Monthly Spending",
                 style: TextStyle(fontSize: 18),
               ),
-              Flexible(
-                child: charts.PieChart(
-                  [
-                    charts.Series<CategoryMonthlyPctAccumulator, String>(
-                      id: 'Adjusted Monthly Spending',
-                      data: _categorySpendingThisMonth,
-                      domainFn: (data, _) => data.categoryName,
-                      measureFn: (data, _) => data.amount,
-                      labelAccessorFn: (data, _) =>
-                          "${data.categoryName}\n${data.amount.toStringAsFixed(1)}%",
-                      colorFn: (_, index) => _colors[index],
-                    )
-                  ],
-                  defaultRenderer: new charts.ArcRendererConfig(
-                    arcRendererDecorators: [new charts.ArcLabelDecorator()],
+              Expanded(
+                child: Center(
+                  child: Text(
+                    NumberFormat.simpleCurrency()
+                        .format(_totalAdjustedMonthlySpending),
+                    style: TextStyle(
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ),
             ],
           ),
         ),
+        CategorySpendingPieChart(
+            categorySpendingThisMonth: _categorySpendingThisMonth,
+            colors: _colors),
         Container(
           padding: EdgeInsets.all(16),
           child: Flex(
@@ -216,32 +223,96 @@ class _OverviewPageState extends State<OverviewPage> {
                 style: TextStyle(fontSize: 18),
               ),
               Flexible(
-                child: _categoryMonthlySpending.isEmpty ? Center(child:CircularProgressIndicator()) : charts.TimeSeriesChart(
-                  <charts.Series<MonthlySpendingAccumulator, DateTime>>[
-                    ..._categoryMonthlySpending
-                        .map(
-                          (category, monthlySpendingAcc) => MapEntry(
-                            category,
-                            charts.Series<MonthlySpendingAccumulator, DateTime>(
-                              id: category.name,
-                              data: monthlySpendingAcc,
-                              domainFn: (data, _) => data.month,
-                              measureFn: (data, _) => data.amount,
-                              colorFn: (_, i) => _colors[category.id],
-                            ),
-                          ),
-                        )
-                        .values
-                  ],
-                  primaryMeasureAxis: charts.NumericAxisSpec(
-                    tickFormatterSpec: simpleCurrencyFormatter,
-                  ),
-                ),
+                child: _categoryMonthlySpending.isEmpty
+                    ? Center(child: CircularProgressIndicator())
+                    : charts.TimeSeriesChart(
+                        <charts.Series<MonthlySpendingAccumulator, DateTime>>[
+                          ..._categoryMonthlySpending
+                              .map(
+                                (category, monthlySpendingAcc) => MapEntry(
+                                  category,
+                                  charts.Series<MonthlySpendingAccumulator,
+                                      DateTime>(
+                                    id: category.name,
+                                    data: monthlySpendingAcc,
+                                    domainFn: (data, _) => data.month,
+                                    measureFn: (data, _) => data.amount,
+                                    colorFn: (_, i) => _colors[category.id],
+                                  ),
+                                ),
+                              )
+                              .values
+                        ],
+                        primaryMeasureAxis: charts.NumericAxisSpec(
+                          tickFormatterSpec: simpleCurrencyFormatter,
+                        ),
+                      ),
               ),
             ],
           ),
         ),
       ],
+    );
+  }
+}
+
+class CategorySpendingPieChart extends StatefulWidget {
+  final List<CategoryMonthlyPctAccumulator> categorySpendingThisMonth;
+  final List<charts.Color> colors;
+
+  CategorySpendingPieChart({
+    @required this.categorySpendingThisMonth,
+    @required this.colors,
+  });
+
+  @override
+  _CategorySpendingPieChartState createState() =>
+      _CategorySpendingPieChartState(
+        categorySpendingThisMonth: this.categorySpendingThisMonth,
+        colors: this.colors,
+      );
+}
+
+class _CategorySpendingPieChartState extends State<CategorySpendingPieChart> {
+  final List<CategoryMonthlyPctAccumulator> categorySpendingThisMonth;
+  final List<charts.Color> colors;
+
+  _CategorySpendingPieChartState({
+    @required this.categorySpendingThisMonth,
+    @required this.colors,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: Flex(
+        direction: Axis.vertical,
+        children: [
+          Text(
+            "Adjusted Monthly Spending by Category",
+            style: TextStyle(fontSize: 18),
+          ),
+          Flexible(
+            child: charts.PieChart(
+              [
+                charts.Series<CategoryMonthlyPctAccumulator, String>(
+                  id: 'Adjusted Monthly Spending',
+                  data: categorySpendingThisMonth,
+                  domainFn: (data, _) => data.categoryName,
+                  measureFn: (data, _) => data.amount,
+                  labelAccessorFn: (data, _) =>
+                      "${data.categoryName}\n${data.amount.toStringAsFixed(1)}%",
+                  colorFn: (_, index) => colors[index],
+                )
+              ],
+              defaultRenderer: new charts.ArcRendererConfig(
+                arcRendererDecorators: [new charts.ArcLabelDecorator()],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
